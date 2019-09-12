b4109227e44d6ec08433c5cae1f4c08aa530a4fb
FlyingRaichu <lyubomir.veselinov@gmail.com>
2019-09-12 16:19:10 +0200

    Exercise03b

--- src/com/company/Exercises.java
+++ src/com/company/Exercises.java
@@ -39,8 +39,42 @@ public class Exercises {
         }
     }
 
-    public void Exercise03(double a, double b) {
+    public void Exercise03a(double a, double b) {
         double hypotenuse = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
         System.out.println("The hypotenuse of a triangle with one side " + a + "cm and the other: " + b + "cm is " + hypotenuse + "cm");
+    }
+
+    public void Exercise03b(double x, String currency) {
+        double bgn_to_dkk = x * 3.82;
+        double dkk_to_bgn = x / 3.82;
+        String bgn;
+        String dkk;
+        String sorm;
+        String[] smterms = {"Bulgarian lev", "Bulgarian leva", "Danish krone", "Danish kroner"};
+
+        if (x == 1) {
+            bgn = smterms[0];
+            dkk= smterms[2];
+            sorm = " is";
+        }
+        else {
+            bgn= smterms[1];
+            dkk= smterms[3];
+            sorm = " are";
+        }
+
+        //never use == to compare strings, since == only works if you've pre-defined your strings, equals() checks if every letter from string 1
+        //checks out with every letter from string 2
+        if (currency.equalsIgnoreCase("dkk")) {
+            System.out.println(x + " " +  dkk + sorm + " equivalent to " + dkk_to_bgn + " " + bgn);
+        }
+
+        else if (currency.equalsIgnoreCase("bgn")) {
+            System.out.println(x + " " +  bgn + sorm  + " equivalent to " + bgn_to_dkk + " " + dkk);
+        }
+
+        else {
+            System.out.println("We do not support this currency");
+        }
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	Exercises exercises = new Exercises();
 
-	exercises.Exercise03(4, 9);
+	exercises.Exercise03b(5, "bgn");
+
     }
 }
 
